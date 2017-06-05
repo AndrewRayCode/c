0a919f2b2193eb8339251e47eff9d5fac7d21515
Andrew Ray <me@andrewray.me>
2017-06-04 20:10:55 -0700

    Deploy script update

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,9 @@ fileName=c-${1}.tar.gz
 tar -zcf c-${1}.tar.gz c c_recent_branches_completer
 
 echo "$fileName"
+
+mv $fileName ~/c-releases/
+cd ~/c-releases
+git add $fileName
+git commit -am 'Committing version $1'
+git push origin master
